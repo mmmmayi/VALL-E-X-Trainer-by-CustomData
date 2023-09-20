1680eb3090a8df7112bd408d2a3f876462f0665e
0417keito <0417keito@gmail.com>
2023-09-20 20:07:39 +0900

    solve circular import

--- train_utils/utils.py
+++ train_utils/utils.py
@@ -12,66 +12,11 @@ _lhotse_uuid = None
 import uuid
 import argparse
 import warnings
-import os
-from macros import *
-from data.tokenizer import (
-    AudioTokenizer,
-    tokenize_audio,
-)
-from models.vallex import VALLE
-from vocos import Vocos
 
 # use duck typing for LRScheduler since we have different possibilities, see
 # our class LRScheduler.
 LRSchedulerType = object
 Pathlike = Union[str, Path]
-
-def get_model(device):
-    url = 'https://huggingface.co/Plachta/VALL-E-X/resolve/main/vallex-checkpoint.pt'
-
-    checkpoints_dir = "./checkpoints"
-
-    model_checkpoint_name = "vallex-checkpoint_modified.pt"
-    if not os.path.exists(checkpoints_dir): os.mkdir(checkpoints_dir)
-    if not os.path.exists(os.path.join(checkpoints_dir, model_checkpoint_name)):
-        import wget
-        print("3")
-        try:
-            logging.info(
-                "Downloading model from https://huggingface.co/Plachta/VALL-E-X/resolve/main/vallex-checkpoint.pt ...")
-            # download from https://huggingface.co/Plachta/VALL-E-X/resolve/main/vallex-checkpoint.pt to ./checkpoints/vallex-checkpoint.pt
-            wget.download("https://huggingface.co/Plachta/VALL-E-X/resolve/main/vallex-checkpoint.pt",
-                          out="./checkpoints/vallex-checkpoint.pt", bar=wget.bar_adaptive)
-        except Exception as e:
-            logging.info(e)
-            raise Exception(
-                "\n Model weights download failed, please go to 'https://huggingface.co/Plachta/VALL-E-X/resolve/main/vallex-checkpoint.pt'"
-                "\n manually download model weights and put it to {} .".format(os.getcwd() + "\checkpoints"))
-    # VALL-E
-    model = VALLE(
-        N_DIM,
-        NUM_HEAD,
-        NUM_LAYERS,
-        norm_first=True,
-        add_prenet=False,
-        prefix_mode=PREFIX_MODE,
-        share_embedding=True,
-        nar_scale_factor=1.0,
-        prepend_bos=True,
-        num_quantizers=NUM_QUANTIZERS,
-    ).to(device)
-    checkpoint = torch.load(os.path.join(checkpoints_dir, model_checkpoint_name), map_location='cpu')
-    missing_keys, unexpected_keys = model.load_state_dict(
-        checkpoint["model"], strict=True
-    )
-    assert not missing_keys
-
-    # Encodec
-    codec = AudioTokenizer(device)
-    
-    vocos = Vocos.from_pretrained('charactr/vocos-encodec-24khz').to(device)
-    
-    return model, codec, vocos
 
 def add_model_arguments(parser: argparse.ArgumentParser):
     parser.add_argument(
