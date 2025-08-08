f4ab78ae4cce110cd70a4af74fcc185394b7c696
mmmmayi <e0643891@u.nus.edu>
2025-08-08 16:49:54 +0800

    change dataset

--- data/dataset.py
+++ data/dataset.py
@@ -17,16 +17,21 @@
 """
 modified from lhoste.dataset.speech_synthesis.py
 """
-
+import os
 import torch
 import math
-import h5py
-from tokenizers import Tokenizer
+#from tokenizers import Tokenizer
 from typing import Union, List
 import numpy as np
 from tqdm import tqdm
-from utils.g2p import PhonemeBpeTokenizer
+#from utils.g2p import PhonemeBpeTokenizer
 from data.collation import get_text_token_collater
+from fairseq.data import Dictionary
+from fairseq.data import (
+    data_utils,
+    indexed_dataset,
+)
+from fairseq import utils
 text_collater = get_text_token_collater()
 
 _pad        = '_'
@@ -34,11 +39,7 @@ _punctuation = ',.!?-~…'
 _letters = 'NQabdefghijklmnopstuvwxyzɑæʃʑçɯɪɔɛɹðəɫɥɸʊɾʒθβŋɦ⁼ʰ`^#*=ˈˌ→↓↑ '
 symbols = [_pad] + list(_punctuation) + list(_letters)
 
-language_dict = {
-    'en': 0,
-    'zh': 1,
-    'ja': 2,
-}
+
 def seq2phone(tokens: Union[List, np.ndarray]):
     """
     Convert tokenized phoneme ID sequence back to phoneme string
@@ -49,7 +50,7 @@ def seq2phone(tokens: Union[List, np.ndarray]):
     return phones
 
 class DynamicBatchSampler(torch.utils.data.Sampler):
-    def __init__(self, sampler, num_tokens_fn, num_buckets=100, min_size=0, max_size=1000,
+    def __init__(self, set_lang, sampler, num_tokens_fn, num_buckets=100, min_size=0, max_size=1000,
                  max_tokens=None, max_sentences=None, drop_last=False):
         """
 
@@ -63,6 +64,7 @@ class DynamicBatchSampler(torch.utils.data.Sampler):
         super(DynamicBatchSampler, self).__init__(sampler)
         self.sampler = sampler
         self.num_tokens_fn = num_tokens_fn
+        self.set_lang = set_lang
         self.num_buckets = num_buckets
 
         self.min_size = min_size
@@ -76,6 +78,7 @@ class DynamicBatchSampler(torch.utils.data.Sampler):
         self.drop_last = drop_last
 
     def set_epoch(self, epoch):
+        self.set_lang()
         self.sampler.set_epoch(epoch)
     def is_batch_full(self, num_tokens, batch):
         if len(batch) == 0:
@@ -87,6 +90,7 @@ class DynamicBatchSampler(torch.utils.data.Sampler):
         return False
 
     def __iter__(self):
+        
         buckets = [[] for _ in range(self.num_buckets)]
         sample_len = [0] * self.num_buckets
 
@@ -132,71 +136,112 @@ class DynamicBatchSampler(torch.utils.data.Sampler):
 
 
 class AudioDataset(torch.utils.data.Dataset):
-    def __init__(self, h5_path, ann_path, tokenizer_path):
-        self.h5_path = h5_path
-        with open(ann_path, 'r', encoding='utf-8') as f:
-            lines = f.readlines()
-        ls = [l.split("|") for l in lines]
-        ls_T = list(zip(*ls))
-        #del ls_T[-1]
-        self.h5_paths, self.durations, self.langs, self.texts = \
-            list(ls_T[0]), list(ls_T[1]), list(ls_T[2]), list(ls_T[3])
-        self.durations = [float(dur) for dur in self.durations]
-        self.tokenizer = PhonemeBpeTokenizer(tokenizer_path)
-        self._archive = None
+    def __init__(self, data_dir, lang=['zh','en'],num_quantizers=8):
+        super().__init__()
+        self.data_dir = data_dir
+        self.num_quantizers = num_quantizers
+        self.at_dict = Dictionary.load(os.path.join(self.data_dir, "checkpoint/slam_vallex/pretrained_model", "dict.at.txt"))
+        self.st_dict = Dictionary.load(os.path.join(self.data_dir, "checkpoint/slam_vallex/pretrained_model","dict.st.txt"))
+       
+        self.at_dict.tts_flag = self.at_dict.add_symbol("<TTS>")
+        self.st_dict.asr_flag = self.st_dict.add_symbol("<ASR>")
+        self.at_dict.mt_flag = self.st_dict.add_symbol("<MT>")
+        assert len(lang)>0, "lang must be in ['zh','en']"
+        self.len=0
+        self.lang_id_dict = {}
+        if 'zh' in lang:
+            self.zh_at_dataset, self.zh_st_dataset, self.zh_dur, self.zh_datanum = self.load_data("data_wenet")
+            self.len=self.zh_datanum
+            self.lang_id_dict["zh"] = 0
+            self.lang = "zh"
+        if 'en' in lang:
+            self.en_at_dataset, self.en_st_dataset, self.en_dur, self.en_datanum = self.load_data("data_libri_12s")
+            self.len = max(self.len,self.en_datanum)
+            self.lang_id_dict["en"] = 1
+            self.lang = "en"
+
+    def load_data(self, prefix):
+        at_dataset=[]
+        for i in range(self.num_quantizers):
+            temp_data_path = os.path.join(
+            self.data_dir, prefix,"data_bin_at_{}".format(i),'train'
+            )
+            at_dataset.append(data_utils.load_indexed_dataset(temp_data_path, self.at_dict, None))
+
+        temp_data_path = os.path.join(
+            self.data_dir, prefix,"data_bin_st",'train'
+        )
+        assert indexed_dataset.dataset_exists(temp_data_path, impl=None), temp_data_path
+        st_dataset = data_utils.load_indexed_dataset(temp_data_path, self.st_dict, None)
+        
+        temp_data_path = os.path.join(
+            self.data_dir, prefix,"dur",'train'
+        )
+        assert indexed_dataset.dataset_exists(temp_data_path, impl=None), temp_data_path
+        dur_dataset = data_utils.load_indexed_dataset(temp_data_path, None, None)
+
+        assert len(at_dataset) == len(st_dataset) == len(dur_dataset), "at_dataset and st_dataset have different lengths"
+        return at_dataset, st_dataset, dur_dataset,len(st_dataset)
 
     def __len__(self):
-        return len(self.h5_paths)
+        return self.len
 
     def get_dur(self, idx):
-        return self.durations[idx]
+        if self.lang == "zh":
+            return float(self.zh_dur[idx].item())
+        if self.lang == "en":
+            return float(self.en_dur[idx].item())
 
-    @property
-    def archive(self):
-        if self._archive is None:  # lazy loading here!
-            self._archive = h5py.File(self.h5_path, "r")
-        return self._archive
-    def __getitem__(self, idx):
-        archive = self.archive
-        h5_path = self.h5_paths[idx]
-        sub = archive[h5_path]
-        audio_tokens = sub['audio'][()]
-        #phone_tokens = sub['text'][()]
-        dur = self.durations[idx]
-        lang = self.langs[idx]
-        text = self.texts[idx]
-        # tokenization should be done within dataloader
-        #phones = seq2phone(phone_tokens)
-        #phones = phones.replace(" ", "_")
-        phonemes, langs = self.tokenizer.tokenize(text=f"{text}".strip())
-        cptpho_tokens, enroll_x_lens = text_collater([phonemes])
+    def set_lang(self):
+        """随机选择一种语言并赋值给 self.lang。"""
+        if not self.lang_id_dict:
+            return
+        keys = list(self.lang_id_dict.keys())
+        self.lang = str(np.random.choice(keys))
+
+    def get_at(self,idx,data):
+        """
+        从 8 个量化子数据集中取出相同 idx 的序列，直接在列维拼接成 [L, 8] 的张量；
+        :param idx: 样本索引
+        :param data: 长度为 8 的数据集列表，每个元素支持 __getitem__(idx)
+        :return: torch.LongTensor，形状 [L, 8]
+        """
+        sequences = []
+        for ds in data:
+            seq = ds[idx]
+            if isinstance(seq, np.ndarray):
+                seq = torch.from_numpy(seq)
+            elif not torch.is_tensor(seq):
+                seq = torch.tensor(seq)
+            seq = seq.long().view(-1)
+            sequences.append(seq)
+
+        assert len(sequences)>0 , "sequences is empty"
+        return torch.stack(sequences, dim=1)
+
+    def __getitem__(self, index):
+        if self.lang == "zh":
+            idx = index%self.zh_datanum
+            at = self.get_at(idx,self.zh_at_dataset)
+            st = self.zh_st_dataset[idx]
+
+        if self.lang == "en":  
+            idx = index%self.en_datanum
+            at = self.get_at(idx,self.en_at_dataset)
+            st = self.en_st_dataset[idx]
+
+        cptpho_tokens, enroll_x_lens = text_collater([st])
         cptpho_tokens = cptpho_tokens.squeeze(0)
         text_token_lens = enroll_x_lens[0]
-        '''
-        if not len(phones):
-            cptpho_tokens = self.tokenizer.encode(text).ids
-        else:
-            cptpho_tokens = self.tokenizer.encode(phones).ids
-        '''
-        assert len(cptpho_tokens)
         return {
-            'utt_id': h5_path,
-            'text': text,
-            'audio': None,
-            'audio_lens': None,
-            'audio_features': audio_tokens,
-            'audio_features_lens': audio_tokens.shape[1],
-            'text_tokens': np.array(cptpho_tokens),
-            'text_tokens_lens': text_token_lens,
-            'language': language_dict[lang],
+                "audio_features": at,
+                "audio_features_lens": at.shape[0],
+                "text_tokens": np.array(cptpho_tokens),
+                "text_tokens_lens": text_token_lens,
+                "language": self.lang_id_dict[self.lang]
         }
 
 def collate(batch):
-    utt_id_s = [b['utt_id'] for b in batch]
-    text_s = [b['text'] for b in batch]
-
-    audio_s = [b['audio'] for b in batch]
-    audio_lens_s = [b['audio_lens'] for b in batch]
 
     audio_features_lens_s = [b['audio_features_lens'] for b in batch]
     # create an empty tensor with maximum audio feature length
@@ -218,10 +263,6 @@ def collate(batch):
         text_tokens_s[i, :text_tokens_lens] = torch.LongTensor(text_tokens)
 
     batch = {
-        'utt_id': utt_id_s,
-        'text': text_s,
-        'audio': audio_s,
-        'audio_lens': audio_lens_s,
         'audio_features': audio_features_s,
         'audio_features_lens': torch.LongTensor(np.array(audio_features_lens_s)),
         'text_tokens': text_tokens_s,
@@ -230,17 +271,15 @@ def collate(batch):
     }
     return batch
 
-def create_dataloader(data_dir="/root/valle/egs/mix", n_gpus=1, rank=0, num_workers=0, num_buckets=10, max_duration=120):
-    train_dataset = AudioDataset(h5_path=f"{data_dir}/audio_sum.hdf5",
-                                 ann_path=f"{data_dir}/audio_ann_sum.txt",
-                                 tokenizer_path=f"{data_dir}/bpe_69.json")
+def create_dataloader(data_dir, lang, n_gpus=1, rank=0, num_workers=0, num_buckets=10, max_duration=120):
+    train_dataset = AudioDataset(data_dir=data_dir,lang=lang)
     ran_sampler = torch.utils.data.distributed.DistributedSampler(
             train_dataset,
             num_replicas=n_gpus,
             rank=rank,
             shuffle=True,
         )
-    dynamic_sampler = DynamicBatchSampler(ran_sampler, train_dataset.get_dur, num_buckets=num_buckets, max_size=20,
+    dynamic_sampler = DynamicBatchSampler(train_dataset.set_lang, ran_sampler, train_dataset.get_dur, num_buckets=num_buckets, max_size=20,
                                           max_tokens=max_duration)
 
 
